6085a96cda065e7f84917ff97f346548fe9df678
oupiseyit <oupisey.it@gmail.com>
2020-02-09 20:08:54 +0700

    - Add route api for cocktails.

---
+++ src/Controller/CocktailsController.php
@@ -0,0 +1,117 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Cocktails Controller
+ *
+ *
+ * @method \App\Model\Entity\Cocktail[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class CocktailsController extends AppController
+{
+    // /**
+    //  * Index method
+    //  *
+    //  * @return \Cake\Http\Response|null
+    //  */
+    // public function index()
+    // {
+    //     $cocktails = $this->paginate($this->Cocktails);
+
+    //     $this->set(compact('cocktails'));
+    // }
+
+    // /**
+    //  * View method
+    //  *
+    //  * @param string|null $id Cocktail id.
+    //  * @return \Cake\Http\Response|null
+    //  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+    //  */
+    // public function view($id = null)
+    // {
+    //     $cocktail = $this->Cocktails->get($id, [
+    //         'contain' => [],
+    //     ]);
+
+    //     $this->set('cocktail', $cocktail);
+    // }
+
+    // /**
+    //  * Add method
+    //  *
+    //  * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+    //  */
+    // public function add()
+    // {
+    //     $cocktail = $this->Cocktails->newEntity();
+    //     if ($this->request->is('post')) {
+    //         $cocktail = $this->Cocktails->patchEntity($cocktail, $this->request->getData());
+    //         if ($this->Cocktails->save($cocktail)) {
+    //             $this->Flash->success(__('The cocktail has been saved.'));
+
+    //             return $this->redirect(['action' => 'index']);
+    //         }
+    //         $this->Flash->error(__('The cocktail could not be saved. Please, try again.'));
+    //     }
+    //     $this->set(compact('cocktail'));
+    // }
+
+    // /**
+    //  * Edit method
+    //  *
+    //  * @param string|null $id Cocktail id.
+    //  * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+    //  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+    //  */
+    // public function edit($id = null)
+    // {
+    //     $cocktail = $this->Cocktails->get($id, [
+    //         'contain' => [],
+    //     ]);
+    //     if ($this->request->is(['patch', 'post', 'put'])) {
+    //         $cocktail = $this->Cocktails->patchEntity($cocktail, $this->request->getData());
+    //         if ($this->Cocktails->save($cocktail)) {
+    //             $this->Flash->success(__('The cocktail has been saved.'));
+
+    //             return $this->redirect(['action' => 'index']);
+    //         }
+    //         $this->Flash->error(__('The cocktail could not be saved. Please, try again.'));
+    //     }
+    //     $this->set(compact('cocktail'));
+    // }
+
+    // /**
+    //  * Delete method
+    //  *
+    //  * @param string|null $id Cocktail id.
+    //  * @return \Cake\Http\Response|null Redirects to index.
+    //  * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+    //  */
+    // public function delete($id = null)
+    // {
+    //     $this->request->allowMethod(['post', 'delete']);
+    //     $cocktail = $this->Cocktails->get($id);
+    //     if ($this->Cocktails->delete($cocktail)) {
+    //         $this->Flash->success(__('The cocktail has been deleted.'));
+    //     } else {
+    //         $this->Flash->error(__('The cocktail could not be deleted. Please, try again.'));
+    //     }
+
+    //     return $this->redirect(['action' => 'index']);
+    // }
+
+    public $paginate = [
+        'page' => 1,
+        'limit' => 5,
+        'maxLimit' => 100,
+        'fields' => [
+            'id', 'name', 'description'
+        ],
+        'sortWhitelist' => [
+            'id', 'name', 'description'
+        ]
+    ];
+}
